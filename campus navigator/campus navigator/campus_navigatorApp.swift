c270789f8d9c8eeefedefb1af9e13ef0d76aa2f1
gamika <gpunsisi14@gmail.com>
2025-02-18 16:05:28 +0530

    updates

---
+++ campus navigator/campus navigator/campus_navigatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  campus_navigatorApp.swift
+//  campus navigator
+//
+//  Created by Gamika Punsisi on 2025-02-18.
+//
+
+import SwiftUI
+
+@main
+struct campus_navigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ campus navigator/campus navigatorTests/campus_navigatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  campus_navigatorTests.swift
+//  campus navigatorTests
+//
+//  Created by Gamika Punsisi on 2025-02-18.
+//
+
+import Testing
+@testable import campus_navigator
+
+struct campus_navigatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
